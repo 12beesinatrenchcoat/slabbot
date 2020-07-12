85befa6d86fb20897617f08b2862c4613c400c13
AndyThePie <25379179+AndyThePie@users.noreply.github.com>
2020-07-12 13:10:18 -0400

    change "slabbot about" message
    avant-garde => weird
    added version number (to be updated on version releases)

--- commands/meta.js
+++ commands/meta.js
@@ -12,10 +12,10 @@ class SlabbotAbout extends Command{
     exec(message){
         let uptime = (this.client.uptime);
 
-        return message.channel.send(`**[ = ' x ' = ] slabbot** is an experimental discord bot with avant-garde features (that no one ever asked for). 
+        return message.channel.send(`**[ = ' x ' = ] slabbot** is an experimental discord bot with weird features (that no one ever asked for). 
 it is created by \`@AndzCLiv3#7664\`, and its source code can be found here: https://github.com/AndyThePie/slabbot
 
-slabbot has been up for **${uptime}ms**.`);
+slabbot has been up for **${uptime}ms** / running version \`dev\`.`);
     }
 }
 
