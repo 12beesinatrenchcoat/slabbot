34261b75c3cd77b76c567ba6e78de5c6add2e236
AndyThePie <andyclive3602@gmail.com>
2020-09-26 15:21:49 -0400

    added "decide" command
    give it a list of things, separated by commas. it chooses one.

---
+++ commands/utilities/decide.js
@@ -0,0 +1,32 @@
+// give slabbot some things, and they'll choose one.
+
+const { Command } = require("discord-akairo");
+const info = require("../meta/commandinfo.json")
+
+class Decide extends Command {
+    constructor() {
+        super("decide", {
+            aliases: ["decide"],
+            category: "utilties",
+            description: info.decide,
+            args: [
+                {
+                    id: "items",
+                    match: "content",
+                }
+            ]
+        });
+    }
+
+    exec(message, args) {
+        if(!args.items) {
+            return message.reply("you gave me nothing to work with here...");
+        }
+
+        const items = args.items.split(",");
+        const picked = items[Math.floor(Math.random() * items.length)]
+        return message.reply(`I choose ${picked}!`)
+    }
+}
+
+module.exports = Decide
