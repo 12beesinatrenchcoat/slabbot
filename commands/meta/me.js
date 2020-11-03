f18a263b038391674bdb95bf7bc75d3561cd7a36
AndyThePie <andyclive3602@gmail.com>
2020-11-03 16:45:28 -0500

    better looking `slabbot me`
    compact mobile that's all nice and mobile friendly. hopefully.
    more detailed / desktop version later,  as well as maybe better progress bar.

--- commands/meta/me.js
+++ commands/meta/me.js
@@ -1,11 +1,33 @@
 // stuff about the user
 
 const { Command } = require("discord-akairo");
+const { MessageEmbed } = require("discord.js");
+
 const info = require("./commandinfo.json");
 const userModel = require("../../model.user.js");
 
 // see also: https://www.desmos.com/calculator/kcrt4evjgg
 const expNeededForLevel = level => 1024*(level**1.3)+(level/35)**4.5;
+
+async function createExpBar(percentage, maxLength) {
+    let output = "";
+    const fillCount = Math.floor(percentage / maxLength);
+
+    for(let char = 0; char < maxLength; char++){
+
+        if (char === 0) {
+            output += "[";
+        } else if (char <= fillCount) {
+            output += "|";
+        } else if (char === maxLength - 1) {
+            output += "]";
+        } else {
+            output += " ";
+        }
+    }
+
+    return output;
+}
 
 class SlabbotMe extends Command{
     constructor() {
@@ -28,7 +50,25 @@ class SlabbotMe extends Command{
         const expForNextLevel = expNeededForLevel(level + 1);
         const percentToNextLevel = ((exp - expForCurrentLevel)/expForNextLevel*100);
 
-        return message.reply(`you are currently level ${level} (${Math.round(percentToNextLevel*100)/100}% of the way to the next level) and have ${Math.round(exp * 1000) / 1000} exp.`);
+        // compact/lite version of card.
+        const expBar = await createExpBar(percentToNextLevel, 10);
+        const embed = new MessageEmbed()
+            .setColor("#f69321")
+            .setTitle(`\`me\` / ${message.author.tag}`)
+            .setDescription(
+                `
+\`\`\`glsl
+# LEVEL ${level}
+${expBar}${Math.round(percentToNextLevel*10)/10}%
+${Math.round(exp)} exp
+\`\`\`
+                `
+            )
+            .setThumbnail(message.author.avatarURL())
+            .setFooter("compact ver.")
+            .setTimestamp(message.createdTimestamp);
+
+        return message.channel.send(`here you go, <@!${message.author.id}>!`, embed);
     }
 }
 
