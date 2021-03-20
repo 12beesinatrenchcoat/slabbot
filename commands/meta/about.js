0277dc91f53a7f8245388ec4a897f6aee53b7585
AndyThePie <andyclive3602@gmail.com>
2021-03-20 02:16:52 -0400

    `sl about` > removed intents + extra line breaks
    goodness gracious fields look disgusting on mobile.

--- commands/meta/about.js
+++ commands/meta/about.js
@@ -27,15 +27,15 @@ class SlabbotAbout extends Command{
         const embed = new MessageEmbed()
             .setColor("#f69321")
             .setTitle("[= ^ x ^ =] hello!")
-            .setDescription("**slabbot** (<@!729861979677917214>) is an experimental discord bot with weird features (that no one ever asked for).\n\nit is being created by `@12beesinatrenchcoat#7664` / <@!231899170716385280>, as a really questionable way to learn how to program in JavaScript.\n\nthe bot is also open source. source code here. https://github.com/AndyThePie/slabbot")
+            .setDescription("**slabbot** (<@!729861979677917214>) is an experimental discord bot with weird features (that no one ever asked for).\nit is being created by `@12beesinatrenchcoat#7664` / <@!231899170716385280>, as a really questionable way to learn how to program in JavaScript.\nthe bot is also open source. source code here. https://github.com/AndyThePie/slabbot")
             .setThumbnail("https://raw.githubusercontent.com/AndyThePie/slabbot/master/images/slabbot-icon.png")
             .addField("Powered by", "discord.js + Akairo\nStack Overflow", true)
             .addField("Current Uptime",`${Math.round(uptime/10)/100}s`,true)
             .addField("Total Commands Run",totalCommands,true)
             .addField("Most Used Commands",`
-            1. \`${sortedStats[0]._id}\` (used ${sortedStats[0].value} times)
-            2. \`${sortedStats[1]._id}\` (used ${sortedStats[1].value} times)
-            3. \`${sortedStats[2]._id}\` (used ${sortedStats[2].value} times)
+1. \`${sortedStats[0]._id}\` (used ${sortedStats[0].value} times)
+2. \`${sortedStats[1]._id}\` (used ${sortedStats[1].value} times)
+3. \`${sortedStats[2]._id}\` (used ${sortedStats[2].value} times)
             `,true);
 
         return message.reply("here's some information about me!", embed);
