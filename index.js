3140d767f2c1c94f385deeec4d881190735717b6
AndyThePie <12beesinatrenchcoat+gitstuff@protonmail.com>
2021-05-21 14:11:21 -0400

    update dependencies and some deprecations
    i guess this is what they call a chore, or something. "useFindAndModify" warning still shows up, though, for some reason. can't explain that. (kareem?)
    anyways, resolves #25.

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+// setting up mongoose...
 const mongoose = require("mongoose");
 const url = "mongodb://localhost:27017";
 const db = mongoose.connection;
 require("./bot.js");
 
-mongoose.connect(url, {useNewUrlParser: true});
+mongoose.connect(url, {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function() {
     console.log("connected to database! [...]");  
