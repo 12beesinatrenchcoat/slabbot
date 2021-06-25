164f22f4b1bb46b6fb3b4b48e5feb3ed3ef9f95c
Andy Chan <25379179+AndyThePie@users.noreply.github.com>
2021-06-25 00:45:55 -0400

    refactor `me.js` a bit, welcome new users a bit moved creating the embed into its own function (as i normally do), and also some more varied messages. resolves #23. will need to clean up, though.

--- commands/meta/me.js
+++ commands/meta/me.js
@@ -14,6 +14,42 @@ async function getNickname(message, user) {
 	return guildMember.nickname ?? "";
 }
 
+// dbObject is an object containing stuff from the mongoDB. may change.
+function createMeEmbed(user, nickname, dbObject) {
+	const {exp = 0, level = 0, stats = null} = dbObject;
+
+	const expForCurrentLevel = expNeededForLevel(level);
+	const expForNextLevel = expNeededForLevel(level + 1);
+	const percentToNextLevel = ((exp - expForCurrentLevel) / expForNextLevel * 100);
+
+	const expBar = createExpBar(percentToNextLevel, 44);
+	const bigLevelNumber = toBigNumber(level);
+
+	const description = `
+${nickname ? "also known as `" + nickname + "`" : ""}
+\`\`\`glsl
+# LEVEL ${"—".repeat(((level.toString().length - 1) * 7))}
+${bigLevelNumber}${expBar}
+${percentToNextLevel.toFixed(2)}% (${exp.toFixed(3)} exp)
+${(expForNextLevel - exp).toFixed(3)} to next level
+\`\`\`
+`;
+
+	const embed = new MessageEmbed()
+		.setColor(SLABBOT_ORANGE)
+		.setTitle(`\`me\` ${user.tag}`)
+		.setDescription(description)
+		.setThumbnail(user.avatarURL())
+		.setTimestamp(new Date().getTime());
+	if (stats) {
+		const sortedStats = Object.entries(stats).sort((a, b) => b[1] - a[1]);
+		const totalCommands = Object.values(stats).reduce((a, b) => a + b);
+		embed.addField("Commands Run", `${totalCommands} (mostly \`${fNum(sortedStats[0][0])}\`, ${fNum(sortedStats[0][1])}x)`, true);
+	}
+
+	return embed;
+}
+
 class SlabbotMe extends Command {
 	constructor() {
 		super("me", {
@@ -33,50 +69,29 @@ class SlabbotMe extends Command {
 	}
 
 	async exec(message, args) {
+		let text = "here you go!";
 		const user = args.user ?? message.author;
 		const nickname = await getNickname(message, user);
 
 		// fetch exp, level, and stats (command usage)
 		const {exp, level, stats} = await userModel.findById(user.id, "exp level stats") || 0;
 
-		// TODO: better error handling with new users.
-		if (exp === 0) {
-			return;
+		if (user.bot === true) {
+			return message.reply("that's a bot!... i don't track those.");
 		}
 
-		if (!stats) {
+		if (!stats && user !== message.author) {
 			return message.reply("that user isn't in my database...");
 		}
 
-		const expForCurrentLevel = expNeededForLevel(level);
-		const expForNextLevel = expNeededForLevel(level + 1);
-		const percentToNextLevel = ((exp - expForCurrentLevel) / expForNextLevel * 100);
+		// new user who decides to just use "sl me"
+		if (!stats && user === message.author) {
+			text = `hello <@!${message.author.id}>, i'm slabbot!`;
+		}
 
-		const sortedStats = Object.entries(stats).sort((a, b) => b[1] - a[1]);
-		const totalCommands = Object.values(stats).reduce((a, b) => a + b);
+		const embed = createMeEmbed(user, nickname, {exp, level, stats});
 
-		const expBar = createExpBar(percentToNextLevel, 44);
-		const bigLevelNumber = toBigNumber(level);
-
-		const description = `
-${nickname ? "also known as `" + nickname + "`" : ""}
-\`\`\`glsl
-# LEVEL ${"—".repeat(((level.toString().length - 1) * 7))}
-${bigLevelNumber}${expBar}
-${percentToNextLevel.toFixed(2)}% (${exp.toFixed(3)} exp)
-${(expForNextLevel - exp).toFixed(3)} to next level
-\`\`\`
-`;
-
-		const embed = new MessageEmbed()
-			.setColor(SLABBOT_ORANGE)
-			.setTitle(`\`me\` ${user.tag}`)
-			.setDescription(description)
-			.setThumbnail(user.avatarURL())
-			.setTimestamp(message.createdTimestamp)
-			.addField("Commands Run", `${totalCommands} (mostly \`${fNum(sortedStats[0][0])}\`, ${fNum(sortedStats[0][1])}x)`, true);
-
-		return message.channel.send(`here you go, <@!${message.author.id}>!`, embed);
+		return message.channel.send(text, embed);
 	}
 }
 
