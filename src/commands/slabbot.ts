7a9e7e2cc93f90d601d24f87fcd4849e93033483
Andy Chan (12beesinatrenchcoat) <andy@12beesinatrenchco.at>
2025-01-03 01:21:51 -0500

    refactor: Split slabbot subcommands into separate functions
    Hopefully less of a pain to look through in the future. Also makes
    eslint happy.

--- src/commands/slabbot.ts
+++ src/commands/slabbot.ts
@@ -32,130 +32,137 @@ export default class implements Command {
 	execute = async function (interaction: ChatInputCommandInteraction, client: Client) {
 		const subcommand = interaction.options.getSubcommand();
 
-		/* /slabbot about */
-		if (subcommand === "about") {
-			if (!client.user || !client.uptime) {
-				return logger.error("client.user or client.uptime is null… which really shouldn't happen.");
-			}
-
-			const slabbotStats = await getSlabbotStats();
-
-			const embed = new EmbedBuilder()
-				.setColor(colors.orange)
-				.setTitle("[= ^ x ^ =] hello!")
-				.setDescription("Hello! I'm slabbot (<@" + client.user.id + ">)!\n"
-					+ "I was created by @12beesinatrenchcoat (<@231899170716385280>).\n"
-					+ "You can find my source code [here](https://github.com/12beesinatrenchcoat/slabbot).")
-			// TODO: Replace this image.
-				.setThumbnail("https://raw.githubusercontent.com/AndyThePie/slabbot/master/images/slabbot-icon.png")
-				.addFields({
-					name: "Uptime",
-					value: msToDuration(client.uptime) as string,
-					inline: true,
-				});
-
-			if (slabbotStats) {
-				embed.addFields({
-					name: "Commands Run",
-					value: slabbotStats.commandsRun,
-					inline: true,
-				}, {
-					name: "Most Used Commands",
-					value: slabbotStats.mostUsedCommands,
-					inline: false,
-				});
-			}
-
-			return interaction.reply({
-				content: "oh hi! [= ^ x ^ =]",
-				embeds: [embed],
-			});
-		}
-
-		/* /slabbot profile */
-		if (subcommand === "profile") {
-			if (mongoose.connection.readyState !== 1) {
-				logger.trace("no database, no stats");
-				return interaction.reply({
-					content: "database missing…",
-					embeds: [
-						generateCommandProblemEmbed(
-							"database not connected",
-							"there's no database connected, and therefore no stats to get. contact the person running the bot.",
-							"error",
-						),
-					],
-				});
-			}
-
-			let member: GuildMember | null = null;
-			let user: User;
-
-			// In server or in DMs
-			if (interaction.inGuild()) {
-				member = interaction.options.getMember("user") as GuildMember || interaction.member;
-				user = member.user;
-			} else {
-				user = interaction.options.getUser("user", false) || interaction.user;
-			}
-
-			if (!user) {
-				return false;
-			}
-
-			const {id} = user;
-
-			if (user.bot) {
-				return interaction.reply({
-					content: "nah, i don't do bots",
-					embeds: [botProfileError],
-					ephemeral: true,
-				});
-			}
-
-			await interaction.deferReply();
-
-			let databaseUser = await UsersModel.findById(id) as SlabbotUser;
-
-			databaseUser ||= await newUser(id);
-
-			const {
-				exp = 0,
-				level = 0,
-				commandUsage = new Map<string, number>(),
-				stats = new Map<string, number>(), // TODO: Do something with this?
-			} = databaseUser;
-
-			let description = "";
-
-			if (member?.nickname) {
-				description += "also known as `" + member.nickname + "`\n";
-			}
-
-			description += levelProgress(level, exp);
-
-			const embed = new EmbedBuilder()
-				.setTitle(user.tag.replace("#0", ""))
-				.setDescription(description)
-				.setThumbnail(user.avatarURL() || user.defaultAvatarURL)
-				.setTimestamp(new Date());
-
-			if ([...commandUsage.entries()].length > 0) {
-				const mostUsedCommand = [...commandUsage.entries()].reduce((a, b) => b[1] > a[1] ? b : a);
-				const totalCommands = [...commandUsage.values()].reduce((a, b) => a + b);
-				embed.addFields({
-					name: "Commands Run",
-					value: `${totalCommands} (mostly /${mostUsedCommand[0]}, ${mostUsedCommand[1]}x)`,
-					inline: true,
-				});
-			}
-
-			return interaction.editReply({
-				content: "here you go!",
-				embeds: [embed],
-			});
+		switch (subcommand) {
+			case "about":
+				return subcommandAbout(interaction, client);
+			case "profile":
+				return subcommandProfile(interaction);
+			default:
+				logger.error(`Unrecognized subcommand ${subcommand} in slabbot.`);
 		}
 	};
+}
+
+async function subcommandAbout(interaction: ChatInputCommandInteraction, client: Client) {
+	if (!client.user || !client.uptime) {
+		return logger.error("client.user or client.uptime is null… which really shouldn't happen.");
+	}
+
+	const slabbotStats = await getSlabbotStats();
+
+	const embed = new EmbedBuilder()
+		.setColor(colors.orange)
+		.setTitle("[= ^ x ^ =] hello!")
+		.setDescription("Hello! I'm slabbot (<@" + client.user.id + ">)!\n"
+			+ "I was created by @12beesinatrenchcoat (<@231899170716385280>).\n"
+			+ "You can find my source code [here](https://github.com/12beesinatrenchcoat/slabbot).")
+	// TODO: Replace this image.
+		.setThumbnail("https://raw.githubusercontent.com/AndyThePie/slabbot/master/images/slabbot-icon.png")
+		.addFields({
+			name: "Uptime",
+			value: msToDuration(client.uptime) as string,
+			inline: true,
+		});
+
+	if (slabbotStats) {
+		embed.addFields({
+			name: "Commands Run",
+			value: slabbotStats.commandsRun,
+			inline: true,
+		}, {
+			name: "Most Used Commands",
+			value: slabbotStats.mostUsedCommands,
+			inline: false,
+		});
+	}
+
+	return interaction.reply({
+		content: "oh hi! [= ^ x ^ =]",
+		embeds: [embed],
+	});
+}
+
+async function subcommandProfile(interaction: ChatInputCommandInteraction) {
+	if (mongoose.connection.readyState !== 1) {
+		logger.trace("no database, no stats");
+		return interaction.reply({
+			content: "database missing…",
+			embeds: [
+				generateCommandProblemEmbed(
+					"database not connected",
+					"there's no database connected, and therefore no stats to get. contact the person running the bot.",
+					"error",
+				),
+			],
+		});
+	}
+
+	let member: GuildMember | null = null;
+	let user: User;
+
+	// In server or in DMs
+	if (interaction.inGuild()) {
+		member = interaction.options.getMember("user") as GuildMember || interaction.member;
+		user = member.user;
+	} else {
+		user = interaction.options.getUser("user", false) || interaction.user;
+	}
+
+	if (!user) {
+		return false;
+	}
+
+	const {id} = user;
+
+	if (user.bot) {
+		return interaction.reply({
+			content: "nah, i don't do bots",
+			embeds: [botProfileError],
+			ephemeral: true,
+		});
+	}
+
+	await interaction.deferReply();
+
+	let databaseUser = await UsersModel.findById(id) as SlabbotUser;
+
+	databaseUser ||= await newUser(id);
+
+	const {
+		exp = 0,
+		level = 0,
+		commandUsage = new Map<string, number>(),
+		stats = new Map<string, number>(), // TODO: Do something with this?
+	} = databaseUser;
+
+	let description = "";
+
+	if (member?.nickname) {
+		description += "also known as `" + member.nickname + "`\n";
+	}
+
+	description += levelProgress(level, exp);
+
+	const embed = new EmbedBuilder()
+		.setTitle(user.tag.replace("#0", ""))
+		.setDescription(description)
+		.setThumbnail(user.avatarURL() || user.defaultAvatarURL)
+		.setTimestamp(new Date());
+
+	if ([...commandUsage.entries()].length > 0) {
+		const mostUsedCommand = [...commandUsage.entries()].reduce((a, b) => b[1] > a[1] ? b : a);
+		const totalCommands = [...commandUsage.values()].reduce((a, b) => a + b);
+		embed.addFields({
+			name: "Commands Run",
+			value: `${totalCommands} (mostly /${mostUsedCommand[0]}, ${mostUsedCommand[1]}x)`,
+			inline: true,
+		});
+	}
+
+	return interaction.editReply({
+		content: "here you go!",
+		embeds: [embed],
+	});
 }
 
 /**
